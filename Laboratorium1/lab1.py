e3df303d496de2df750fb70703151d7770236104
Kuban001 <jakub.grabowski223@gmail.com>
2025-06-12 13:18:12 +0200

    Update lab1.py

--- Laboratorium1/lab1.py
+++ Laboratorium1/lab1.py
@@ -1,1 +1,19 @@
+import math
 
+# Funkcja zip() łączy elementy z dwóch lub więcej iterowalnych obiektów 
+# (np. list, krotek) w pary. Zwraca obiekt zip, który można przekonwertować na listę lub inny typ.
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+zipped = list(zip(list1, list2))  #https://docs.python.org/3/library/functions.html#zip
+print("Zipped lists:", zipped)
+
+#Moduł Math
+digit = 16
+square_root = math.sqrt(digit)
+print(f"Square root of  {digit}: {square_root}")
+
+# Błąd ZeroDivisionError występuje gdy próbujemy podzielić liczbę przez zero. 
+try:
+    result = 10 / 0  #https://docs.python.org/3/library/exceptions.html#ZeroDivisionError
+except ZeroDivisionError:
+    print("Cannot divide by zero")
